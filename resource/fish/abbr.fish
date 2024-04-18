351e5c32d8dee4cf7f8c771e18f33ef9f5deb42c
Jimmie Fulton <jimmie.fulton@gmail.com>
2024-04-17 21:38:54 -0700

    Add 'grh' and 'gcl' abbreviations

--- resource/fish/abbr.fish
+++ resource/fish/abbr.fish
@@ -1,11 +1,20 @@
 abbr -a ga git add
 abbr -a ga. git add .
 abbr -a gi d git init
+abbr -a gc git commit
+abbr -a gcm git commit -m
+abbr -a gcl git clone
+abbr -a gclp d git clone -p
+abbr -a gco git checkout
+abbr -a gp git checkout
+abbr -a gl git log
 abbr -a gip d git init -p
 abbr -a gar d git add-remote
 abbr -a garp d git add-remote -p
 abbr -a gd git diff
 abbr -a gdc git diff --cached
+abbr -a grh git reset --hard
+abbr -a gcl git clean -df
 abbr -a gs git status
 abbr -a gpu git pull
 abbr -a gp git push
@@ -15,14 +24,6 @@ abbr -a a archetect
 abbr -a acm archetect cache manage
 abbr -a rr rustrover
 abbr -a i idea
-abbr -a ga git add
-abbr -a ga. git add .
-abbr -a gc git commit
-abbr -a gcl git clone
-abbr -a gclp d git clone -p
-abbr -a gco git checkout
-abbr -a gp git checkout
-abbr -a gl git log
 abbr -a mcp mvn clean package
 abbr -a mcps mvn clean package -DskipTests
 abbr -a mci mvn clean install
